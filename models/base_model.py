1368f3fa2c48b8b8d3c44a0bbafeda9c9be0fa34
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-10-11 19:19:52 -0300

    implementación de abstracción

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,39 @@
+from abc import ABC, abstractmethod
+from typing import List
+
 from helpers.file_helpers import write_json_file, read_json_file
 
 
-class Base:
-    all: list = []
-    filename = ''
+class Base(ABC):
+    all: List['Base'] = []
+    filename: str = None
+
+    @abstractmethod
+    def parse_to_dict(self) -> dict:
+        pass
+
+    @classmethod
+    def __check_filename(cls):
+        if (cls.filename is None) or (not cls.filename.endswith('.json')):
+            raise Exception('Cada clase hija de Base debe settear el atributo de clase "filename"')
 
     @classmethod
     def save(cls):
-        write_json_file(cls.filename, [
-                        todo.__dict__ for todo in cls.all])
+        cls.__check_filename()
+        write_json_file(cls.filename, [item.parse_to_dict() for item in cls.all])
+
+    @classmethod
+    @abstractmethod
+    def parse_from_dict(cls, item:dict) -> 'Base':
+        pass
 
     @classmethod
     def load(cls):
-        for todo_dict in read_json_file(cls.filename):
-            todo_obj = cls(name=todo_dict['name'], description=todo_dict['description'], done=todo_dict['done'])
-            cls.all.append(todo_obj)
+        cls.__check_filename()
+        
+        item_list: list[dict] = read_json_file(cls.filename)
+        # for item in read_json_file(cls.filename):
+        #     # item_obj = cls(name=item['name'], description=item['description'], done=item['done'])
+        #     item_obj = cls.parse_from_dict(item)
+        #     cls.all.append(item_obj)
+        cls.all = [cls.parse_from_dict(item) for item in item_list]
--- models/todo_model.py
+++ models/todo_model.py
@@ -14,17 +14,29 @@ class Todo(Base):
     @property
     def name(self):
         return self.__name.title()
-    
+
     @name.setter
     def name(self, new_name):
         if not new_name.strip():
             raise Exception('Invalid name')
         self.__name = new_name
 
-    @property
-    def __dict__(self):
+    # @property
+    # def __dict__(self):
+    #     return {
+    #         'name': self.__name,
+    #         'description': self.description,
+    #         'done': self.done,
+    #     }
+
+    def parse_to_dict(self) -> dict:
         return {
             'name': self.__name,
             'description': self.description,
             'done': self.done,
         }
+
+    @classmethod
+    def parse_from_dict(cls, item: dict) -> 'Todo':
+        # return cls(name=item['name'], description=item['description'], done=item['done'])
+        return cls(**item)
